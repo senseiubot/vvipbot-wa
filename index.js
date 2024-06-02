4f0251a141c3e56aafcb886903f7b8e96d66bc6a
senseiubot <100754362+senseiubot@users.noreply.github.com>
2024-06-02 10:00:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const instagram = 'agus_alnsyh71- kaelMk_';
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Agus\n' // KUAT MENTAL
+            + 'ORG:VVIPINDO;\n' // OSCAR
+            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //+6285172060015
             + 'END:VCARD'
 const
 {
